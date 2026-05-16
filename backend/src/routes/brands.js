14ec94cd3071da98f3732bfc4a319c051e158297
Hegu911 <hegu911@gmail.com>
2026-05-16 21:42:41 +0400

    Delete backend directory

--- backend/src/routes/brands.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllBrands, getBrandBySlug } = require('../controllers/brandController');
-
-router.get('/', getAllBrands);
-router.get('/:slug', getBrandBySlug);
-
-module.exports = router;
--- backend/src/routes/categories.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllCategories, getCategoryBySlug } = require('../controllers/categoryController');
-
-router.get('/', getAllCategories);
-router.get('/:slug', getCategoryBySlug);
-
-module.exports = router;
